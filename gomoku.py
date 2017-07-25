0a2efea21688106022fef73bf188d281d0719890
David Garner <dgarner@mintel.com>
2017-07-25 09:25:22 +0100

    Start checking column and row victory.

--- gomoku.py
+++ gomoku.py
@@ -1,9 +1,37 @@
 from client import GomokuBase
 
+
 class Gomoku(GomokuBase):
     """
     Write your bot here!
     """
+    def get_victor(self, state):
+        # Check along rows.
+        for row in range(self.SIZE):
+            offset = row * self.SIZE
+            in_a_row = 0
+            for col in range(1, self.SIZE):
+                if state[offset + col] == state[offset + col - 1] != 0:
+                    in_a_row += 1
+                else:
+                    in_a_row = 0
+                if in_a_row == 4:
+                    return state[offset + col]
+
+        # Check along columns.
+        for col in range(self.SIZE):
+            in_a_row = 0
+            for offset in range(self.SIZE, self.SIZE * self.SIZE, self.SIZE):
+                if state[offset + col] == state[offset + col - self.SIZE] != 0:
+                    in_a_row += 1
+                else:
+                    in_a_row = 0
+                if in_a_row == 4:
+                    return state[offset + col]
+
+        # TODO diagonals
+
+        return 0
 
     def play_turn(self, state, player):
         """
@@ -15,4 +43,5 @@ class Gomoku(GomokuBase):
         return sum(abs(i) for i in state)
 
 
-Gomoku()
+if __name__ == '__main__':
+    Gomoku()
