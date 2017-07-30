bc1a4d714ed39eae9027e8a20c76fa169a692e70
David Garner <dgarner@mintel.com>
2017-07-30 23:24:28 +0100

    Remove code that will probably never be used

--- gomoku.py
+++ gomoku.py
@@ -22,71 +22,6 @@ FIVES = {
 
 class Gomoku(GomokuBase):
 
-    def get_victor(self, state):
-        """
-        TODO use np.diag
-        """
-
-        # Check along rows.
-        for row in range(self.SIZE):
-            in_a_row = 0
-            for col in range(1, self.SIZE):
-                if state[row, col] == state[row, col - 1] != 0:
-                    in_a_row += 1
-                else:
-                    in_a_row = 0
-                if in_a_row == 4:
-                    return state[row, col]
-
-        # Check along columns.
-        for col in range(self.SIZE):
-            in_a_row = 0
-            for row in range(self.SIZE):
-                if state[row, col] == state[row - 1, col] != 0:
-                    in_a_row += 1
-                else:
-                    in_a_row = 0
-                if in_a_row == 4:
-                    return state[row, col]
-
-        # The down-right diagonals.
-        for i in range(-self.SIZE + 5, self.SIZE - 4):
-            start_row = max(0, -i)
-            start_col = max(0, i)
-
-            last = None
-            in_a_row = 0
-
-            for k in range(min(self.SIZE - start_row, self.SIZE - start_col)):
-                next_square = state[start_row + k, start_col + k]
-                if next_square == last != 0:
-                    in_a_row += 1
-                else:
-                    in_a_row = 0
-                if in_a_row == 4:
-                    return last
-                last = next_square
-
-        # The up-right diagonals.
-        for i in range(4, self.SIZE * 2 - 5):
-            start_row = min(i, self.SIZE - 1)
-            start_col = max(0, i - self.SIZE + 1)
-
-            last = None
-            in_a_row = 0
-
-            for k in range(min(start_row + 1, self.SIZE - start_col)):
-                next_square = state[start_row - k, start_col + k]
-                if next_square == last != 0:
-                    in_a_row += 1
-                else:
-                    in_a_row = 0
-                if in_a_row == 4:
-                    return last
-                last = next_square
-
-        return 0
-
     def match_six_threat(self, counters):
         # Return (player, type, defence_squares, offense_squares).
 
@@ -146,9 +81,6 @@ class Gomoku(GomokuBase):
             del line[0]
 
     def find_threats_in_grid(self, state):
-        """
-        In progress...
-        """
         threats = []
 
         # Check along rows for a threat.
