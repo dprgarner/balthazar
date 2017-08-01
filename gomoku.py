4a659f243b83bb92912cc04449f7a84be3a63533
David Garner <dprgarner@gmail.com>
2017-08-01 20:32:57 +0100

    First go at a pretty rough heuristic function

--- gomoku.py
+++ gomoku.py
@@ -33,8 +33,36 @@ FIVES = {
 class Gomoku(GomokuBase):
     # Tuneable parameters
     centre_weight = 1
-    centre_gradient = 1
+    centre_gradient = 0.1
     cross_weight = 1
+
+    potential_three_player = 150
+    potential_three_opponent = 75
+
+    potential_two_player = 20
+    potential_two_opponent = 15
+
+    potential_one_player = 3
+    potential_one_opponent = 1
+
+    @property
+    def potential_weights(self):
+        if not hasattr(self, '_potential_weights'):
+            self._potential_weights = {
+            # Could certainly form a threat next turn.
+            (3, 2, 0): self.potential_three_opponent,
+            (0, 2, 3): self.potential_three_player,
+
+            # Could potentially form a threat next turn.
+            (2, 3, 0): self.potential_two_opponent,
+            (0, 3, 2): self.potential_two_player,
+
+            # Perhaps better to play in places that could eventually form a
+            # five.
+            (1, 4, 0): self.potential_one_opponent,
+            (0, 4, 1): self.potential_one_player,
+        }
+        return self._potential_weights
 
     def centre_bias(self):
         # A numpy array reflecting bias towards the centre.
@@ -59,10 +87,71 @@ class Gomoku(GomokuBase):
         cross[self.SIZE // 2 + 1, self.SIZE // 2 + 1] = 1
         return self.cross_weight * cross
 
+    def find_potentials_in_line(self, line, player):
+        counts = [0, 0, 0]
+        for i in range(len(line)):
+            counts[player * line[i] + 1] += 1
+            if i > 4:
+                counts[line[i - 5] + 1] -= 1
+
+            if i > 3:
+                weight = self.potential_weights.get(tuple(counts), None)
+                if weight:
+                    yield i - 4, weight
+
+    def add_possible_fives_bias(self, state, player):
+        biases = np.zeros((self.SIZE, self.SIZE), dtype=float)
+
+        # Check along rows for potential fives.
+        for row in range(self.SIZE):
+            for offset, weight in self.find_potentials_in_line(
+                state[row],
+                player,
+            ):
+                for i in range(5):
+                    biases[row, offset + i] += weight
+
+        # Check along columns...
+        for col in range(self.SIZE):
+            for offset, weight in self.find_potentials_in_line(
+                state[:, col],
+                player,
+            ):
+                for i in range(5):
+                    biases[offset + i, col] += weight
+
+        # Check along down-right diagonals...
+        for diag_offset in range(-self.SIZE + 5, self.SIZE - 4):
+            diag_line = list(np.diagonal(state, offset=diag_offset))
+            for offset, weight in self.find_potentials_in_line(
+                diag_line,
+                player,
+            ):
+                row = max(0, -diag_offset) + offset
+                col = max(0, diag_offset) + offset
+                for i in range(5):
+                    biases[row + i, col + i] += weight
+
+        # Check along down-right diagonals...
+        for diag_offset in range(-self.SIZE + 5, self.SIZE - 4):
+            diag_line = list(np.diagonal(state[::-1], offset=diag_offset))
+            for offset, weight in self.find_potentials_in_line(
+                diag_line,
+                player,
+            ):
+                row = self.SIZE - 1 - max(0, -diag_offset) - offset
+                col = max(0, diag_offset) + offset
+                for i in range(5):
+                    biases[row - i, col + i] += weight
+
+
+        return biases
+
     def get_heuristic_board(self, state, player):
         heuristic = np.zeros((self.SIZE, self.SIZE), dtype=float)
         heuristic += self.centre_bias()
         heuristic += self.cross_bias()
+        heuristic += self.add_possible_fives_bias(state, player)
         heuristic[state != 0] = 0
         return heuristic
 
--- test_heuristics.py
+++ test_heuristics.py
@@ -53,3 +53,104 @@ class TestHeuristics(unittest.TestCase):
         """)
         turn = Gomoku().play_turn(board, -1)
         self.assertIn(turn, [(6, 6), (8, 8), (8, 6), (6, 8)])
+
+
+class TestPossibleFivesHeuristics(unittest.TestCase):
+
+    def test_possible_fives(self):
+        """
+        Check that the same rows, columns, and diagonals of an existing square
+        are preferred.
+        """
+        board = parse_board("""
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . X . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+        """)
+
+        class GomokuWithBias(Gomoku):
+            potential_one_player = 1
+            potential_one_opponent = 0
+
+        biases = GomokuWithBias().add_possible_fives_bias(board, 1)
+        # The centre is a nonsense result, so ignore.
+        biases[7, 7] = 0
+        expected = np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 2, 3, 4, 0, 4, 3, 2, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ], dtype=float)
+        self.assertTrue(np.all(biases == expected))
+
+    def test_possible_fives(self):
+        """
+        Check that the same rows, columns, and diagonals of an opponent's square
+        are preferred.
+        """
+        board = parse_board("""
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . X . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . .
+        """)
+
+        class GomokuWithBias(Gomoku):
+            potential_one_player = 0
+            potential_one_opponent = 1
+
+        biases = GomokuWithBias().add_possible_fives_bias(board, -1)
+        # The centre is a nonsense result, so ignore.
+        biases[7, 7] = 0
+        expected = np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 2, 3, 4, 0, 4, 3, 2, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ], dtype=float)
+        self.assertTrue(np.all(biases == expected))
